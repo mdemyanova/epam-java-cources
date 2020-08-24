bc933fa88bcd363af04404c1e44358c9c6239da1
Maria Demyanova <korchagina.adv@gmail.com>
2020-08-24 22:30:56 +0300

    Task004 done.

---
+++ src/main/java/com/epam/university/java/core/task004/Task004Impl.java
@@ -0,0 +1,40 @@
+package com.epam.university.java.core.task004;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+public class Task004Impl implements Task004 {
+    @Override
+    public String[] intersection(String[] first, String[] second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException();
+        }
+
+        ArrayList<String> intersect = new ArrayList<>();
+        int i, j;
+        for (i = 0; i < first.length; i++) {
+            for (j = 0; j < second.length; j++) {
+                if (first[i].equals(second[j])) {
+                    intersect.add(first[i]);
+                }
+            }
+        }
+        return intersect.toArray(new String[0]);
+    }
+
+    @Override
+    public String[] union(String[] first, String[] second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException();
+        }
+        LinkedHashSet<String> union = new LinkedHashSet<>();
+        int i, j;
+        for (i = 0; i < first.length; i++) {
+            union.add(first[i]);
+        }
+        for (j = 0; j < second.length; j++) {
+            union.add(second[j]);
+        }
+        return union.toArray(new String[0]);
+    }
+}

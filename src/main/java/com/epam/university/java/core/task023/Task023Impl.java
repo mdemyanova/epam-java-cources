2fe2f80321f6c2d81c746490da9920dc4a8214e6
Maria Demyanova <korchagina.adv@gmail.com>
2020-09-09 17:37:46 +0300

    Task023 in progress.

---
+++ src/main/java/com/epam/university/java/core/task023/Task023Impl.java
@@ -0,0 +1,12 @@
+package com.epam.university.java.core.task023;
+
+public class Task023Impl implements Task023 {
+    @Override
+    public String extract(String phoneString) {
+        if (phoneString == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return null;
+    }
+}

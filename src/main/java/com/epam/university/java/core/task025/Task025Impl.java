87ee5aacf829559fe26439231ce60800fdce1471
Maria Demyanova <korchagina.adv@gmail.com>
2020-09-09 20:47:45 +0300

    Task025 done.

---
+++ src/main/java/com/epam/university/java/core/task025/Task025Impl.java
@@ -0,0 +1,29 @@
+package com.epam.university.java.core.task025;
+
+public class Task025Impl implements Task025 {
+    @Override
+    public int getAmountOfAlteredLetters(String sourceMessage) {
+        if (sourceMessage == null) {
+            throw new IllegalArgumentException();
+        }
+        if (sourceMessage.isEmpty()) {
+            return 0;
+        }
+        int i;
+        int counter = 0;
+        for (i = 0; i < sourceMessage.length(); i = i + 3) {
+            String sos = sourceMessage.substring(i, i + 3);
+            if (sos.charAt(0) != 'S') {
+                counter++;
+            }
+            if (sos.charAt(1) != 'O') {
+                counter++;
+            }
+            if (sos.charAt(2) != 'S') {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}

adc610ff8a3cdef870e6f4c480b61e150bc74b01
Maria Demyanova <korchagina.adv@gmail.com>
2020-10-04 17:47:29 +0300

    Task027 done.

---
+++ src/main/java/com/epam/university/java/core/task027/Task027Impl.java
@@ -0,0 +1,34 @@
+package com.epam.university.java.core.task027;
+
+import java.util.*;
+
+public class Task027Impl implements Task027 {
+    @Override
+    public Collection<Integer> extract(String sourceString) {
+        if (sourceString == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 1; i <= sourceString.length() / 2; i++) {
+            List<Integer> integers = new ArrayList<>();
+            String sequence = sourceString.substring(0, i);
+            if (sequence.startsWith("0")) {
+                break;
+            }
+
+            int ai = Integer.parseInt(sequence);
+            integers.add(ai);
+            int counter = 1;
+            while (sequence.length() < sourceString.length()) {
+                sequence = sequence.concat(String.valueOf(ai + counter));
+                integers.add(ai + counter);
+                counter++;
+            }
+            if (sourceString.equals(sequence)) {
+                return integers;
+            }
+        }
+
+        return new ArrayList<>();
+    }
+}

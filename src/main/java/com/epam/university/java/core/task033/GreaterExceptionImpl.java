48370a19996a9fe5e420b7c3ded307b1c8d95602
Maria Demyanova <korchagina.adv@gmail.com>
2020-10-22 22:47:03 +0300

    Task 033 done.

--- src/main/java/com/epam/university/java/core/task033/GreaterExceptionImpl.java
+++ src/main/java/com/epam/university/java/core/task033/GreaterExceptionImpl.java
@@ -1,6 +1,7 @@
 package com.epam.university.java.core.task033;
 
-public class GreaterExceptionImpl extends RuntimeException implements GreaterException, BaseException {
+public class GreaterExceptionImpl extends RuntimeException implements GreaterException,
+        BaseException {
 
     GreaterExceptionImpl(String message) {
         super(message, new BaseExceptionImpl());
